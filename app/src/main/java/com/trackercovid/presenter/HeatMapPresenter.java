efdb5ffcb6668dd2a0b59573e69b95fd940c7117
Riza Nafis <riza.nafis@gmail.com>
2020-12-23 14:32:05 +0700

    Create HeatMap Presenter Fixes #26

---
+++ app/src/main/java/com/trackercovid/presenter/HeatMapPresenter.java
@@ -0,0 +1,43 @@
+package com.trackercovid.presenter;
+
+import com.trackercovid.callback.RepositoryCallback;
+import com.trackercovid.contract.HeatMapContract;
+import com.trackercovid.interactor.CountryRepository;
+import com.trackercovid.model.Country;
+
+import java.util.List;
+
+public class HeatMapPresenter implements HeatMapContract.Presenter {
+
+    private final HeatMapContract.View view;
+    private final CountryRepository repository;
+
+    public HeatMapPresenter(HeatMapContract.View view, CountryRepository repository) {
+        this.view = view;
+        this.repository = repository;
+    }
+
+    @Override
+    public void requestHeatMapData() {
+        view.startLoading();
+        repository.getCountries(new RepositoryCallback<List<Country>>() {
+            @Override
+            public void onSuccess(List<Country> data) {
+                view.stopLoading();
+                view.showHeatMapData(data);
+            }
+
+            @Override
+            public void onEmpty() {
+                view.stopLoading();
+                view.showError("No HeatMap Data Found.");
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+                view.stopLoading();
+                view.showError(errorMessage);
+            }
+        });
+    }
+}
